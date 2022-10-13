2176c241d580a0fdc1562028f353dbf64d037b38
Marco <emme.milani@hotmail.it>
2022-10-13 11:54:42 +0200

    dev: program and help

--- stored2model/stored2model.runner/Program.cs
+++ stored2model/stored2model.runner/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.CompilerServices;
+using stored2model.core;
 
-Console.WriteLine("Hello, World!");
+if (args.Length == 0 || args[0] == "-h" || args.Length <3)
+{
+    Console.WriteLine("arg0: connectionString");    
+    Console.WriteLine("arg1: storedProcedure name");    
+    Console.WriteLine("arg2: outputFileName");    
+    Console.WriteLine("arg3[optional]: bool use nullable - default is false");    
+    return;
+}
+
+var connectionString = args[0];
+var storedName =  args[1];
+var outPutFile = args[2];
+var useNullable = args.Length > 3 && bool.Parse(args[3]);
+
+var class1 = new Worker(connectionString, storedName,outPutFile,useNullable);
+class1.Do();
+
+Console.WriteLine($"{outPutFile} written!");
